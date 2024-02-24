d89119939d7808b7f4ae4b5c6f70208606bc1696
TooIneptNeedToLearn <moltencore23@gmail.com>
2024-02-25 02:59:08 +0800

    changed from table into clickable link but the template does not load still

--- Thesis/content/urls.py
+++ Thesis/content/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_thesis),
+    path('', views.show_thesis, name='thesis_list'),
     path('search/', views.thesis_search, name='thesis_search'),
+    path('thesis/<int:thesis_id>/', views.show_thesis_detail, name='thesis_detail'),
 ]
--- Thesis/content/views.py
+++ Thesis/content/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Thesis
 
 def show_thesis(request):
     theses = Thesis.objects.all()
     return render(request, 'list.html', {'theses': theses})
+
+def show_thesis_detail(request, thesis_id):
+    thesis = get_object_or_404(Thesis, pk=thesis_id)
+    return render(request, 'inner_working/thesis_detail.html', {'thesis': thesis})
 
 def thesis_search(request):
     if 'q' in request.GET:
@@ -12,5 +16,3 @@ def thesis_search(request):
     else:
         theses = Thesis.objects.all()
     return render(request, 'list.html', {'theses': theses})
-
-

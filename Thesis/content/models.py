a3b12bdc6248e46f4b31610e4c242eba161013da
TooIneptNeedToLearn <moltencore23@gmail.com>
2024-03-04 16:58:48 +0800

    updated the model

--- Thesis/content/models.py
+++ Thesis/content/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.utils.text import slugify
+from django.utils import timezone
 
 class Thesis(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True, blank=True)  
     authors = models.CharField(max_length=100)
     abstract = models.TextField()
+    body = models.TextField()
     panelist = models.CharField(max_length=100)
     advisors = models.CharField(max_length=255)
     department = models.CharField(max_length=100)  
     defense_date = models.DateField()
-    published_date = models.DateField()
+    published_date = models.DateField(default=timezone.now)
     keywords = models.CharField(max_length=255)
     status = models.CharField(
         max_length=2,
@@ -21,6 +23,12 @@ class Thesis(models.Model):
         ],
         default="UR"
     )
+
+    class Meta:
+        ordering = ['-published_date']
+        indexes = [
+            models.Index(fields=['-published_date'])
+        ]
 
     def __str__(self):
         return self.title
